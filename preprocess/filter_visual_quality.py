c026e0fe24bb5302054d8587218766b805704e32
Oliver <fengfusen@outlook.com>
2025-01-08 11:36:29 +0800

    fix multi gpu

--- preprocess/filter_visual_quality.py
+++ preprocess/filter_visual_quality.py
@@ -61,7 +61,7 @@ def func(paths, device_id):
     model_hyper.train(False)
 
     # load the pre-trained model on the koniq-10k dataset
-    model_hyper.load_state_dict((torch.load("checkpoints/auxiliary/koniq_pretrained.pkl")))
+    model_hyper.load_state_dict((torch.load("checkpoints/auxiliary/koniq_pretrained.pkl", map_location=device)))
 
     transforms = torchvision.transforms.Compose(
         [
@@ -78,7 +78,7 @@ def func(paths, device_id):
             paras = model_hyper(video_frames)  # 'paras' contains the network weights conveyed to target network
 
             # Building target network
-            model_target = TargetNet(paras).cuda()
+            model_target = TargetNet(paras).to(device)
             for param in model_target.parameters():
                 param.requires_grad = False
 
